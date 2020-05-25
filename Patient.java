466161228818e0667a3fbd38591c5f00df02545e
Moris Kücük <mrminibus1@gmail.com>
2020-05-25 16:34:05 +0200

    Erstellung der Klasse Patient

---
+++ Patient.java
@@ -0,0 +1,14 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Patient.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Patient
+{
+    public Patient()
+    {
+
+    }
+}
